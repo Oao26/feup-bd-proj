7e4c71d6244846c794a5f25402c3e8f329750af5
José Santos <92716441+Sereno1710@users.noreply.github.com>
2023-03-22 15:12:11 +0000

    Cleaning Project

--- Desktop/ultimaentregaldts/project-l06gr03-main/src/main/java/com/l06gr03/snake/model/game/elements/Wall.java
+++
@@ -1,7 +0,0 @@
-package com.l06gr03.snake.model.game.elements;
-
-public class Wall extends Element {
-    public Wall(int x, int y) {
-        super(x, y);
-    }
-}
--- Desktop/ultimaentregaldts/project-l06gr03-main/src/main/java/com/l06gr03/snake/viewer/game/ElementViewer.java
+++
@@ -1,8 +0,0 @@
-package com.l06gr03.snake.viewer.game;
-
-import com.l06gr03.snake.gui.GUI;
-import com.l06gr03.snake.model.game.elements.Element;
-
-public interface ElementViewer<T extends Element> {
-    void draw(T element, GUI gui);
-}
